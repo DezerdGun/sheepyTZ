d1617aa4bda6775ca570dc824e17bd0d9e9c8d01
DezerdGun <mir6raid@gmail.com>
2025-08-23 19:06:47 +0500

    fixed encr..

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-
 if [ ! -d "/var/www/vendor" ]; then
     echo "Устанавливаем зависимости Composer..."
     composer install --optimize-autoloader
@@ -11,23 +10,22 @@ if [ ! -f "/var/www/.env" ]; then
     cp /var/www/.env.example /var/www/.env
 fi
 
-if ! php artisan key:generate --show | grep -q 'base64:'; then
-    echo "APP_KEY отсутствует, генерируем..."
-    php artisan key:generate --ansi
-fi
-
 echo "Ожидаем доступность базы данных..."
 until pg_isready -h "${DB_HOST:-db}" -p "${DB_PORT:-5432}" -U "${DB_USERNAME:-postgres}"; do
-    sleep 2
+    echo "Ожидание БД..."
+    sleep 5
 done
 
 echo "Применяем миграции и сиды..."
 php artisan migrate --seed --force || {
     echo "Ошибка при миграциях/сидах, пробуем установить Faker и повторно..."
-    composer require fakerphp/faker --dev
     php artisan migrate --seed --force
 }
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+if ! php artisan key:generate --show | grep -q 'base64:'; then
+    echo "APP_KEY отсутствует, генерируем..."
+    php artisan key:generate --ansi
+fi
 exec php-fpm
